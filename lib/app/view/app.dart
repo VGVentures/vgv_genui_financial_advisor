7f205a15a513c830429d45e14594d3c6f2635476
Noe Fernandez <91338515+noe-xceptions@users.noreply.github.com>
2026-02-06 13:14:13 -0300

    chore: remove counter feature boilerplate

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,4 +1,3 @@
-import 'package:finance_app/counter/counter.dart';
 import 'package:finance_app/l10n/l10n.dart';
 import 'package:flutter/material.dart';
 
@@ -16,7 +15,7 @@ class App extends StatelessWidget {
       ),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      home: const CounterPage(),
+      home: const Scaffold(),
     );
   }
 }
