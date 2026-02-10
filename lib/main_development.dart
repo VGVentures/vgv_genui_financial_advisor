29f35a7aaabb6bbd997952d8421a25732b8741a5
PauliCrespi <crespipauli@gmail.com>
2026-02-10 17:05:14 -0300

    fix: removed unused imports

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,8 +1,7 @@
 import 'package:finance_app/app/app.dart';
 import 'package:finance_app/bootstrap.dart';
+import 'package:finance_app/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_ai/firebase_ai.dart';
-import 'firebase_options.dart';
 
 Future<void> main() async {
   await Firebase.initializeApp(
