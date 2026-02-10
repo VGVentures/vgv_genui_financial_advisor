6027163fd5b31a12150d0fe0deaba9c796d93c6c
Brian Egan <brian.egan@verygood.ventures>
2026-02-10 15:14:34 +0000

    feat: Add wiredash feedback to project

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,22 +1,27 @@
 import 'package:finance_app/counter/counter.dart';
 import 'package:finance_app/l10n/l10n.dart';
 import 'package:flutter/material.dart';
+import 'package:wiredash/wiredash.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+    return Wiredash(
+      projectId: 'gcn26-finance-app-j9k7f4b',
+      secret: 'p_iCQvLnrp18LEacxg6JYRtV5g-FbvfA',
+      child: MaterialApp(
+        theme: ThemeData(
+          appBarTheme: AppBarTheme(
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          ),
+          useMaterial3: true,
         ),
-        useMaterial3: true,
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+        home: const CounterPage(),
       ),
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-      home: const CounterPage(),
     );
   }
 }
