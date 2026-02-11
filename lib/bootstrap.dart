6eb26374cb8dbed583f142326d6b4d0d2cfc1405
Juan Rodríguez <79857303+juanRodriguez17@users.noreply.github.com>
2026-02-11 09:59:53 -0300

    chore: Add crash manager (#5)

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -2,10 +2,13 @@ import 'dart:async';
 import 'dart:developer';
 
 import 'package:bloc/bloc.dart';
+import 'package:finance_app/core/error_reporting_repository/error_reporting_repository.dart';
 import 'package:flutter/widgets.dart';
 
 class AppBlocObserver extends BlocObserver {
-  const AppBlocObserver();
+  const AppBlocObserver({required this.errorReportingRepository});
+
+  final ErrorReportingRepository errorReportingRepository;
 
   @override
   void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
@@ -14,20 +17,30 @@ class AppBlocObserver extends BlocObserver {
   }
 
   @override
-  void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
+  void onError(
+    BlocBase<dynamic> bloc,
+    Object error,
+    StackTrace stackTrace,
+  ) {
     log('onError(${bloc.runtimeType}, $error, $stackTrace)');
+    errorReportingRepository.recordError(error, stackTrace: stackTrace);
     super.onError(bloc, error, stackTrace);
   }
 }
 
-Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
-  FlutterError.onError = (details) {
-    log(details.exceptionAsString(), stackTrace: details.stack);
-  };
+Future<void> bootstrap({
+  required FutureOr<Widget> Function() builder,
+  required ErrorReportingRepository errorReportingRepository,
+}) async {
+  final binding = WidgetsFlutterBinding.ensureInitialized();
+  await errorReportingRepository.init();
 
-  Bloc.observer = const AppBlocObserver();
-
-  // Add cross-flavor configuration here
+  FlutterError.onError = errorReportingRepository.handleFlutterError;
+  binding.platformDispatcher.onError =
+      errorReportingRepository.handlePlatformError;
+  Bloc.observer = AppBlocObserver(
+    errorReportingRepository: errorReportingRepository,
+  );
 
   runApp(await builder());
 }
