79892c332c15cbffef3e2380c48d6777a5e70e33
Noe Fernandez <91338515+noe-xceptions@users.noreply.github.com>
2026-03-04 10:24:15 -0300

    refactor: Update widget exports to include ProgressBar and ProgressBarCatalogPage

--- lib/app/presentation/widgets/widgets.dart
+++ lib/app/presentation/widgets/widgets.dart
@@ -1,3 +1,3 @@
-export 'progress_bar.dart';
 export 'metric_cards.dart';
+export 'progress_bar.dart';
 export 'responsive_scaffold.dart';
--- lib/dev_menu/view/view.dart
+++ lib/dev_menu/view/view.dart
@@ -1,2 +1,3 @@
 export 'design_system_catalog_page.dart';
 export 'metric_card_catalog_page.dart';
+export 'progress_bar_catalog_page.dart';
