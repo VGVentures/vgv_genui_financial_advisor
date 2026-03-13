b803cc1aa2d7a132294ec4149088789b814e9939
Juan Rodríguez <juan.rodriguez@verygood.ventures>
2026-03-13 16:33:21 -0300

    feat: add new run configuration for only showing dev menu apart from the normal flow of the app

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:vgv_genui_financial_advisor/design_system/design_system.dart';
+import 'package:vgv_genui_financial_advisor/feature_flags/feature_flags.dart';
 import 'package:vgv_genui_financial_advisor/l10n/l10n.dart';
 import 'package:vgv_genui_financial_advisor/onboarding/intro/intro.dart';
+import 'package:vgv_genui_financial_advisor/onboarding/onboarding.dart';
 
 class App extends StatelessWidget {
   const App({
     required this.navigatorObservers,
+    this.showDevMenu = false,
     super.key,
   });
 
   final List<NavigatorObserver> navigatorObservers;
+  final bool showDevMenu;
 
   @override
   Widget build(BuildContext context) {
@@ -31,10 +35,51 @@ class App extends StatelessWidget {
 }
 
 class _IntroPage extends StatelessWidget {
-  const _IntroPage();
+  const _IntroPage({this.showDevMenu = false});
+
+  final bool showDevMenu;
 
   @override
   Widget build(BuildContext context) {
-    return const IntroPage();
+    return IntroPage(
+      onGetStarted: () => Navigator.of(context).pushReplacement(
+        MaterialPageRoute<void>(
+          builder: (_) => _StartPage(showDevMenu: showDevMenu),
+        ),
+      ),
+    );
+  }
+}
+
+class _StartPage extends StatelessWidget {
+  const _StartPage({this.showDevMenu = false});
+
+  final bool showDevMenu;
+
+  @override
+  Widget build(BuildContext context) {
+    if (!showDevMenu) {
+      return const Scaffold(body: KickOffPage());
+    }
+
+    final l10n = context.l10n;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(l10n.homeAppBarTitle),
+        actions: [
+          Builder(
+            builder: (context) {
+              return IconButton(
+                icon: const Icon(Icons.bug_report),
+                onPressed: () => Scaffold.of(context).openEndDrawer(),
+              );
+            },
+          ),
+        ],
+      ),
+      endDrawer: const DevMenuDrawer(),
+      body: const KickOffPage(),
+    );
   }
 }
